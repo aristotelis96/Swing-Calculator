13e16d150f182074cb93c15d5fd721579384288a
aristotelis96 <aristotelis96@users.noreply.github.com>
2018-10-09 23:05:06 +0300

    Updated number.java
    removed all int number related, not needed

--- src/swing/Number.java
+++ src/swing/Number.java
@@ -1,21 +1,17 @@
 package swing;
 
 public class Number {
-	private int number;
 	private String number_str;
 	
 	public Number() {
 		number_str = "";
-		number = 0;
 	}
-	public void add(String parseInt) {
+	public void add(String num) {
 		try {
-			number_str+= parseInt;
-			number = Integer.parseInt(number_str);
+			number_str+= num;
 		}
 		catch(Exception e) {
 			number_str = number_str.substring(0, number_str.length()-1);
-			number = Integer.parseInt(number_str);
 		}
 	}
 	
@@ -26,30 +22,25 @@ public class Number {
 	}
 	public void clear() {
 		number_str = "";
-		number = 0;
 	}
 	public void sum(Number num2) {
-		number+=num2.getNumber();
-		number_str = Integer.toString(number);
+		number_str = Integer.toString( num2.getNumber() + Integer.parseInt(number_str) );
+		
 	}
 	public int getNumber() {
 		return Integer.parseInt(number_str);
 	}
 	public void set(int n) {
-		number = n;
 		number_str = Integer.toString(n);
 	}
 	public void sub(Number num2) {
-		number-=num2.getNumber();
-		number_str = Integer.toString(number);		
+		number_str = Integer.toString(Integer.parseInt(number_str) - num2.getNumber());
 	}
 	public void mul(Number num2) {
-		number*=num2.getNumber();
-		number_str = Integer.toString(number);		
+		number_str = Integer.toString( num2.getNumber() * Integer.parseInt(number_str) );
 	}
 	public void div(Number num2) {
-		number/=num2.getNumber();
-		number_str = Integer.toString(number);		
+		number_str = Integer.toString(Integer.parseInt(number_str)/num2.getNumber() );
 	}
 	
 }	
